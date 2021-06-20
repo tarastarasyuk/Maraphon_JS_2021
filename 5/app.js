457408f7c8e7db31bfc00fbc078fb4f8655cdcb7
Taras <worktarasyuk@gmail.com>
2021-06-20 15:30:10 +0300

    add 'hack' feature

--- 5/app.js
+++ 5/app.js
@@ -76,3 +76,13 @@ function getRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min)
 }
 
+function winTheGame() {
+    function kill() {
+        const circle = document.querySelector('.circle')
+
+        if (circle) {
+            circle.click()
+        }
+    }
+    setInterval(kill, 75)
+}
